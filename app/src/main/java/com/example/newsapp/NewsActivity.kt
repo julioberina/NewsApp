3e992026a6e262dd27059087cdfd2067e3d81c51
Julio Berina <julioberina@outlook.com>
2020-07-26 22:34:04 -0700

    Add previous/next page functionality as well as search and reset

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -3,6 +3,9 @@ package com.example.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import java.io.BufferedReader
 import java.io.FileNotFoundException
 import java.io.IOException
@@ -13,6 +16,10 @@ class NewsActivity : AppCompatActivity() {
     companion object {
         var apiKey = ""
     }
+
+    private var currentPage = 1
+    private var currentPageSize = 10
+    private var searchQuery = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,11 +46,55 @@ class NewsActivity : AppCompatActivity() {
         }
     }
 
-    public fun searchClicked() {
+    public fun searchClicked(v: View) {
+        currentPage = 1
+        searchQuery = findViewById<EditText>(R.id.searchText).text.toString().replace(' ', '+')
+        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + currentPage + "-" + (currentPage + 9) + " for:")
+        findViewById<TextView>(R.id.queryText).setText("\"" + searchQuery + "\"")
 
+        val next = findViewById<TextView>(R.id.nextText)
+        next.visibility = View.VISIBLE
+        next.setClickable(true)
     }
 
-    public fun resetClicked() {
+    public fun resetClicked(v: View) {
+        currentPage = 1
+        findViewById<EditText>(R.id.searchText).setText("")
+        findViewById<TextView>(R.id.queryText).setText("")
+        findViewById<TextView>(R.id.showingResultsText).setText("")
 
+        val prev = findViewById<TextView>(R.id.prevText)
+        prev.visibility = View.INVISIBLE
+        prev.setClickable(false)
+
+        val next = findViewById<TextView>(R.id.nextText)
+        next.visibility = View.INVISIBLE
+        next.setClickable(false)
+    }
+
+    public fun prevClicked(v: View) {
+        currentPage -= 10
+
+        if (currentPage == 1) {
+            val prev = findViewById<TextView>(R.id.prevText)
+            prev.visibility = View.INVISIBLE
+            prev.setClickable(false)
+        }
+
+        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + currentPage + "-" + (currentPage + 9) + " for:")
+        findViewById<TextView>(R.id.queryText).setText("\"" + searchQuery + "\"")
+    }
+
+    public fun nextClicked(v: View) {
+        currentPage += 10
+
+        // No limit yet
+
+        val prev = findViewById<TextView>(R.id.prevText)
+        prev.visibility = View.VISIBLE
+        prev.setClickable(true)
+
+        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + currentPage + "-" + (currentPage + 9) + " for:")
+        findViewById<TextView>(R.id.queryText).setText("\"" + searchQuery + "\"")
     }
 }
