3262e5ed98d36772c24a61fa4834bb99877d1fc0
Julio Berina <julioberina@outlook.com>
2020-08-09 20:23:42 -0700

    Implement NewsListAdapter with essentials to load API response data

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -6,10 +6,19 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsapp.data.News
+import com.example.newsapp.interfaces.NewsService
+import kotlinx.android.synthetic.main.activity_news.*
+import kotlinx.coroutines.runBlocking
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.io.BufferedReader
 import java.io.FileNotFoundException
 import java.io.IOException
 import java.io.InputStreamReader
+import kotlin.concurrent.thread
 
 class NewsActivity : AppCompatActivity() {
 
@@ -20,12 +29,41 @@ class NewsActivity : AppCompatActivity() {
     private var currentPage = 1
     private var currentPageSize = 10
     private var searchQuery = ""
+    private var startPage = 1
+    private lateinit var retrofit: Retrofit
+    private lateinit var newsService: NewsService
+    private lateinit var adapter: NewsListAdapter
+    public var data: News? = null
+    public lateinit var defaultData: News
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         if (apiKey == "")   getApiKey()
+
+        retrofit = Retrofit.Builder()
+                            .baseUrl("https://newsapi.org/")
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build()
+
+        newsService = retrofit.create(NewsService::class.java)
+        Log.e("NewsActivity", "I made it to this point")
+
+        runBlocking {
+            val headerMap = mapOf("X-Api-Key" to apiKey)
+            defaultData = newsService.getNews(headerMap, "coronavirus", 10, currentPage)
+        }
+
+        Log.e("NewsActivity", "I finished the request")
+
+        data = defaultData
+        adapter = NewsListAdapter(this, data)
+
         setContentView(R.layout.activity_news)
+
+        val recycler = findViewById<RecyclerView>(R.id.recView)
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this)
     }
 
     private fun getApiKey() {
@@ -48,17 +86,31 @@ class NewsActivity : AppCompatActivity() {
 
     public fun searchClicked(v: View) {
         currentPage = 1
+        startPage = 1
         searchQuery = findViewById<EditText>(R.id.searchText).text.toString().replace(' ', '+')
-        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + currentPage + "-" + (currentPage + 9) + " for:")
+
+        runBlocking {
+            val headerMap = mapOf("X-Api-Key" to apiKey)
+            data = newsService.getNews(headerMap, searchQuery, 10, currentPage)
+        }
+
+        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + startPage + "-" + (startPage + 9) + " for:")
         findViewById<TextView>(R.id.queryText).setText("\"" + searchQuery + "\"")
+
+        adapter.setData(data)
+        adapter.notifyDataSetChanged()
 
         val next = findViewById<TextView>(R.id.nextText)
         next.visibility = View.VISIBLE
         next.setClickable(true)
+
+        val recView = findViewById<RecyclerView>(R.id.recView)
+        recView.visibility = View.VISIBLE
     }
 
     public fun resetClicked(v: View) {
         currentPage = 1
+        startPage = 1
         findViewById<EditText>(R.id.searchText).setText("")
         findViewById<TextView>(R.id.queryText).setText("")
         findViewById<TextView>(R.id.showingResultsText).setText("")
@@ -70,31 +122,55 @@ class NewsActivity : AppCompatActivity() {
         val next = findViewById<TextView>(R.id.nextText)
         next.visibility = View.INVISIBLE
         next.setClickable(false)
+
+        val recView = findViewById<RecyclerView>(R.id.recView)
+        recView.visibility = View.GONE
+
+        data = defaultData
+
+        adapter.setData(data)
+        adapter.notifyDataSetChanged()
     }
 
     public fun prevClicked(v: View) {
-        currentPage -= 10
+        startPage -= 10
+        currentPage -= 1
 
-        if (currentPage == 1) {
+        if (startPage == 1) {
             val prev = findViewById<TextView>(R.id.prevText)
             prev.visibility = View.INVISIBLE
             prev.setClickable(false)
         }
 
-        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + currentPage + "-" + (currentPage + 9) + " for:")
+        runBlocking {
+            val headerMap = mapOf("X-Api-Key" to apiKey)
+            data = newsService.getNews(headerMap, searchQuery, 10, currentPage)
+        }
+
+        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + startPage + "-" + (startPage + 9) + " for:")
         findViewById<TextView>(R.id.queryText).setText("\"" + searchQuery + "\"")
+
+        adapter.setData(data)
+        adapter.notifyDataSetChanged()
     }
 
     public fun nextClicked(v: View) {
-        currentPage += 10
+        startPage += 10
+        currentPage += 1
 
-        // No limit yet
+        runBlocking {
+            val headerMap = mapOf("X-Api-Key" to apiKey)
+            data = newsService.getNews(headerMap, searchQuery, 10, currentPage)
+        }
 
         val prev = findViewById<TextView>(R.id.prevText)
         prev.visibility = View.VISIBLE
         prev.setClickable(true)
 
-        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + currentPage + "-" + (currentPage + 9) + " for:")
+        findViewById<TextView>(R.id.showingResultsText).setText("Showing results " + startPage + "-" + (startPage + 9) + " for:")
         findViewById<TextView>(R.id.queryText).setText("\"" + searchQuery + "\"")
+
+        adapter.setData(data)
+        adapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/newsapp/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsapp/NewsListAdapter.kt
@@ -1,23 +1,69 @@
 package com.example.newsapp
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.data.News
 
-public class NewsListAdapter : BaseAdapter() {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+public class NewsListAdapter() : RecyclerView.Adapter<NewsListAdapter.ViewHolder>() {
+
+    private var data: News? = null
+    private lateinit var context: Context
+
+    constructor(context: Context, data: News?) : this() {
+        this.context = context
+        this.data = data
     }
 
-    override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+    public class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        public lateinit var imageView: ImageView
+        public lateinit var titleText: TextView
+        public lateinit var descriptionText: TextView
+        public lateinit var newsLayout: ConstraintLayout
+
+        init {
+            imageView = itemView.findViewById<ImageView>(R.id.imageView)
+            titleText = itemView.findViewById<TextView>(R.id.titleText)
+            descriptionText = itemView.findViewById<TextView>(R.id.descriptionText)
+            newsLayout = itemView.findViewById<ConstraintLayout>(R.id.newsLayout)
+        }
+
     }
 
-    override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+    public fun setData(data: News?) {
+        this.data = data
     }
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_article, parent, false)
+        val holder = ViewHolder(view)
+        return holder
+    }
+
+    override fun getItemCount(): Int {
+        return data!!.articles.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val article = data!!.articles.get(position)
+        Glide.with(context).load(article?.urlToImage).into(holder.imageView)
+        holder.titleText.setText(article?.title)
+        holder.descriptionText.setText(article?.description)
+
+        holder.newsLayout.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(article?.url))
+            context.startActivity(intent)
+        }
     }
 }
